964af9532c3458aa7acc9e48be87461971df6861
Graham Ollis <plicease@cpan.org>
2017-07-24 16:39:44 -0400

    completish test

--- lib/Alien/Base/Dino.pm
+++ lib/Alien/Base/Dino.pm
@@ -14,9 +14,49 @@ sub import
   # DLLs.
 }
 
+my %dl_path = (
+  darwin  => 'DYLD_LIBRARY_PATH',
+  MSWin32 => 'PATH',
+);
+
+require Env;
+my @dl_path;
+my $dl_path = $dl_path{$^O} || 'LD_LIBRARY_PATH';
+tie @dl_path, 'Env::Array', $dl_path;
+
+=head1 METHODS
+
+=head2 xs_load
+
+ $alien->xs_load($package, $version);
+ $alien->xs_load($package, $version, @other_dino_aliens);
+
+=cut
+
 sub xs_load
 {
-  my($module, $version, @rest) = @_;
+  my($self, $package, $version, @rest) = @_;
+  
+  local $ENV{$dl_path} = $ENV{$dl_path};
+  
+  #if($self->install_type eq 'share')
+  #{
+  #  require Path::Tiny;  
+  #  foreach my $alien ($self, @rest)
+  #  {
+  #    if($alien->can('runtime_prop'))
+  #    {
+  #      my $prop = $alien->runtime_prop;
+  #      if(exists $prop->{dino_path} && ref($prop->{dino_path}) eq 'ARRAY')
+  #      {
+  #        unshift @dl_path, @{ $prop->{dino_path} };
+  #      }
+  #    }
+  #  }
+  #}
+
+  require XSLoader;
+  XSLoader::load($package, $version);
 }
 
 1;
--- t/alien_base_dino.t
+++ t/alien_base_dino.t
@@ -1,6 +1,46 @@
 use Test2::V0 -no_srand => 1;
+use Test::Alien::Build;
+use Test::Alien;
 use Alien::Base::Dino;
 
-isa_ok 'Alien::Base::Dino', 'Alien::Base';
+alienfile_ok filename => 'corpus/libpalindrome.alienfile';
+
+my $alien = alien_build_ok { class => 'Alien::Base::Dino' };
+
+isa_ok $alien, 'Alien::Base';
+isa_ok $alien, 'Alien::Base::Dino';
+
+alien_ok $alien;
+
+xs_ok do { local $/; <DATA> }, with_subtest {
+  my($mod) = @_;
+  is($mod->is_palindrome("hello"), 0);
+  is($mod->is_palindrome("foof"), 1);
+};
+
+run_ok(['palx', 'foo'])
+  ->note
+  ->exit_is(2);
+
+run_ok(['palx', 'foof'])
+  ->note
+  ->success;
 
 done_testing
+
+__DATA__
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+#include <libpalindrome.h>
+
+MODULE = TA_MODULE PACKAGE = TA_MODULE
+
+int
+is_palindrome(klass, word)
+    const char *klass
+    const char *word
+  CODE:
+    RETVAL = is_palindrome(word);
+  OUTPUT:
+    RETVAL
