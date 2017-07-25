cfe77b9dfba66d421b103cc9af68d10608481cc7
Graham Ollis <plicease@cpan.org>
2017-07-25 10:02:50 -0400

    simplify customization on windows

--- lib/Alien/Base/Dino/MSWin32.pm
+++ lib/Alien/Base/Dino/MSWin32.pm
@@ -8,21 +8,8 @@ use Env qw( @PATH );
 sub Alien::Base::Dino::_xs_load_wrapper
 {
   my($self, $code, @rest) = @_;
-
   local $ENV{PATH} = $ENV{PATH};
-
-  foreach my $alien ($self, @rest)
-  {
-    if($alien->can('runtime_prop') && defined $alien->runtime_prop->{rpath})
-    {
-      require Path::Tiny;
-      foreach my $rpath (map { Path::Tiny->new($_)->absolute($alien->dist_dir) } @{ $alien->runtime_prop->{rpath} })
-      {
-        unshift @PATH, $rpath;
-      }
-    }
-  }
-
+  unshift @PATH, $self->rpath(@rest);
   $code->();
 }
 
